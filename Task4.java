37017a57fdcbcdf98dcc620dcc4a1bf17cd319f8
NavyaVajrapu <241801360008@cutmap.ac.in>
2026-01-06 12:32:38 +0530

    Update Task4.java

--- Task4.java
+++ Task4.java
@@ -1,24 +1,43 @@
+class Node{
     int data;
     Node next;
+    
     //constructor
-    Node(int n){
-        data=n;
+    Node(int a){
+        data=a;
         next=null;
-        
     }
+    
     //getdata
     public void getdata(){
-        System.out.println(data);}
+       System.out.println(data); 
+    }
+    
+    //method getData
+    public void getData(){
+        System.out.println(data); 
+    }
+    //method getref
+     public void getRef(){
+        System.out.println(next); 
+    }
+    
 }
 class Main {
     public static void main(String[] args) {
-        System.out.println("NAVYA");
-        System.out.println("Bindu");
-        Node n=new Node(5);
-        Node b=new Node (6);
-        System.out.println(n);
-        n.getdata();
-        b.getdata();
-        
+        System.out.println("navya");
+        Node a=new Node(50);
+        Node b=new Node(100);
+         System.out.println(a);
+         System.out.println(b);
+         a.getdata();
+         b.getdata();
+         
+         a.getRef();
+         a.next=b;
+         a.getRef();
+         
+         
+       
     }
 }
